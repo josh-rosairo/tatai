67e58a2f4d002cd7fe2ea18baf89a08227d7ebc7
anonymous <user@wifi-172-23-81-112.uoa-wifi.auckland.ac.nz>
2017-09-27 14:21:05 +1300

    Levels logic, bash
    - Added function to execute any command in bash
    - Displays number correct at end of level
    - New functionality for replaying a level
    - New functionality for initialising a level
    - Logic for showing/hiding buttons based on number of questions correct
    - Logic for showing/hiding buttons based on if individual question is
    right
    - Now keeps track of number of questions correct, current question

--- TataiController.java
+++ TataiController.java
@@ -1,6 +1,11 @@
 package tatai;
  
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Stack;
 
 import javafx.application.Platform;
@@ -13,19 +18,19 @@ import javafx.stage.Stage;
  
 @SuppressWarnings("unused")
 public class TataiController {
-	// Hashmap of scenes.
-	HashMap<String,Scene> _scenes = new HashMap<String,Scene>();
 	// Stage to swap scenes in and out of.
-	Stage _stage = null;
+	private Stage _stage = null;
 	// Scenes
-	TataiLoader _loader;
-	
-	/**
-	// Uncomment as necessary to access buttons, etc.
-	@FXML private Text number;
-	@FXML private Button level1Button;
-	@FXML private Button level2Button;
-	**/
+	private TataiLoader _loader;
+	// Current level
+	private int _level = 0;
+	// Total number of questions
+	final static private int NUM_QUESTIONS = 10;
+	// Current question number up to
+	private int _currentQuestionNumber = 0;
+	// Number of correct answers this level
+	private int _numCorrect = 0;
+
 	@FXML private Button recordButton;
 	@FXML private Button returnButton;
 	@FXML private Button redoButton;
@@ -34,10 +39,31 @@ public class TataiController {
 	@FXML private Text announceRight;
 	@FXML private Text announceWrong;
 	@FXML private Text numberCorrect;
+	@FXML private Button nextLevelButton;
 	
 	public TataiController(Stage stage) {
 		_stage = stage;
     	_loader = new TataiLoader(this);
+	}
+	
+	// Executes some bash command received as an argument.
+	private List<String> executeCommand(String cmd) {
+		// Read output from the bash.
+		List<String> output = new ArrayList<>();
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		try {
+			Process process = builder.start();
+			InputStream stdout = process.getInputStream();
+			InputStream stderr = process.getErrorStream();
+			BufferedReader stdoutBuffered = new BufferedReader(new InputStreamReader(stdout));
+			String line = null;
+			while ((line = stdoutBuffered.readLine()) != null ) {
+				output.add(line);
+			}
+		} catch (Exception e) {
+			
+		}
+		return output;
 	}
 	
 	public void init() {
@@ -49,11 +75,19 @@ public class TataiController {
 	}
 	    
     @FXML protected void showLevel1(ActionEvent event) {
-        showLevel(1);
+    	_level = 1;
+    	initLevel();
     }
     
     @FXML protected void showLevel2(ActionEvent event) {
-        showLevel(2);
+    	_level = 2;
+    	initLevel();
+    }
+    
+    private void initLevel() {
+    	_numCorrect = 0;
+    	_currentQuestionNumber = 1;
+    	showLevel();
     }
     
     @FXML protected void showMenu(ActionEvent event) {
@@ -63,29 +97,74 @@ public class TataiController {
     }
     
     @FXML protected void record(ActionEvent event) {
+    	// Record and process here.
     	
+    	boolean isCorrect = true;
+    	
+    	// If correct, hide the redo button and increase the number correct.
+    	if (isCorrect) {
+    		_numCorrect++;
+    		announceRight.setVisible(true);
+    		announceRight.setManaged(true);
+    		announceWrong.setVisible(false);
+    		announceWrong.setManaged(false);
+    		redoButton.setVisible(false);
+    		redoButton.setManaged(false);
+    	}
+    	else {
+    		announceWrong.setVisible(true);
+    		announceWrong.setManaged(true);
+    		announceRight.setVisible(false);
+    		announceRight.setManaged(false);
+    		redoButton.setVisible(true);
+    		redoButton.setManaged(true);
+    	}
+    	
+    	// Always show the play button and the next button and hide the record button.
+    	// Show next button.
+    	nextButton.setVisible(true);
+    	nextButton.setManaged(true);
+    	// Show play button.
+    	playButton.setVisible(true);
+    	playButton.setManaged(true);
+    	// Hide record button.
+    	recordButton.setVisible(false);
+    	recordButton.setManaged(false);
     	
     }
     
     @FXML protected void play(ActionEvent event) {
-    	
+    	// Play audio here.
     	
     }
     
     @FXML protected void next(ActionEvent event) {
-    	showEndLevel(1);
+    	if(_currentQuestionNumber >= NUM_QUESTIONS) {
+    		showEndLevel(_numCorrect);
+    	}
+    	else {
+    		// Show the next question.
+    		_currentQuestionNumber++;
+    		showLevel();
+    	}
     }
     
+    // Shows the next level. Only two levels, so show level 2.
     @FXML protected void nextLevel(ActionEvent event) {
-    	
+    	showLevel2(null);
     }
     
+    // Replay current level
     @FXML protected void replay(ActionEvent event) {
-    	
+    	initLevel();
     }
     
-    private void showLevel(int level) {
+    // Show a question for this level.
+    private void showLevel() {
     	Scene scene = _loader.getScene("level");
+    	// Show record button
+    	recordButton.setVisible(true);
+    	recordButton.setManaged(true);
     	// Hide redo button.
     	redoButton.setVisible(false);
     	redoButton.setManaged(false);
@@ -99,12 +178,28 @@ public class TataiController {
     	announceRight.setVisible(false);
     	announceRight.setManaged(false);
     	announceWrong.setVisible(false);
+    	
+    	// Question setup here.
+    	
     	_stage.setScene(scene);
         _stage.show();
     }
     
     private void showEndLevel(int numCorrect) {
+    	// If on level 1 and number correct is greater than or equal to 8, show next level button, else hide it
+    	if (_level == 1 && numCorrect >= 8) {
+    		nextLevelButton.setVisible(true);
+    		nextLevelButton.setManaged(true);
+    	}
+    	else {
+    		nextLevelButton.setVisible(false);
+    		nextLevelButton.setManaged(false);
+    	}
+    	
+    	// Show number correct
     	numberCorrect.setText(Integer.toString(numCorrect));
+    	
+    	
     	Scene scene = _loader.getScene("endlevel");
     	_stage.setScene(scene);
         _stage.show();
